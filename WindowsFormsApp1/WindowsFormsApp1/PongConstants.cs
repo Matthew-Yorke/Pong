cfd6acbb3a4205c8bbbc789ee335b0dae87b6727
Matthew-Yorke <matthew_yorke@yahoo.com>
2017-12-22 14:25:00 -0500

    Add files via upload
    Initial push of Pong written in C# to keep on file.
    
    What's New:
    -Initial graphics laid out for the paddles and ball.
    -Functionality to move paddles up and down.
    
    What's To Come:
    -Separation of classes from the window drawing and game functionality.
    -Ball movement.
    -Collision detection.
    -Score keeping.
    -Starting menu and functionality for that.
    -Pause functionality.
    -Second player A.I. in place of a second physical player

---
+++ WindowsFormsApp1/WindowsFormsApp1/PongConstants.cs
@@ -0,0 +1,49 @@
+﻿//************************************************************************************************************************************************
+//
+// File Name: PongConstants.cs
+//
+// Description:
+//  Constants used throughout the pong game.
+//
+// Change History:
+//  Author               Date           Description
+//  Matthew D. Yorke     12/22/2017     Initial set of constants before adding to github to keep as a backup.
+//
+//************************************************************************************************************************************************
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+   public static class PongConstants
+   {
+      // The width in pixels of any paddle in the game.
+      public const int PADDLE_WIDTH = 10;
+
+      // The height in pixels of any paddle in the game
+      public const int PADDLE_HEIGHT = 200;
+
+      // The padding in pixels between a endzone and any paddle in the game.
+      public const int PADDLE_BOUNDARY_PADDING = 20;
+
+      // The stareting y-coordinate in pixels for any paddle in the game.
+      public const int PADDLE_STARTING_Y_COORDINATE = 0;
+
+      // The speed (number of pixel movement) any paddle in the game can move at.
+      public const int PADDLE_SPEED = 10;
+
+      // THe width and height in pixels the ball in the game is.
+      public const int BALL_WIDTH_AND_HEIGHT = 20;
+
+      // THe number of milliseconds for a clock tick to occur.
+      public const int TIMER_INTERVAL = 16;
+
+      // Used for dividing a value in half.
+      public const int HALF = 2;
+   }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿//************************************************************************************************************************************************
+//
+// File Name: Program.cs
+//
+// Description:
+//  The entrance for the when the program starts. the Main fucntion is located here and indicates to create the window used for pong.
+//
+// Change History:
+//  Author               Date           Description
+//  Matthew D. Yorke     12/22/2017     Initial release of this class that kicks off the program.
+//
+//************************************************************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+   static class Program
+   {
+      //*********************************************************************************************************************************************
+      //
+      // Method Name: Main
+      //
+      // Description:
+      //  The main entrance into the program that kicks off the window for the pong game.
+      //
+      // Arguments:
+      //  N/A
+      //
+      // Return:
+      //  N/A
+      //
+      //*********************************************************************************************************************************************
+      [STAThread]
+      static void Main()
+      {
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         Application.Run(new Form1());
+      }
+   }
+}
